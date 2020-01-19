d6bdcc85517c07350543491efb345ed2dfe7df3a
semklauke <privat@semklauke.de>
2020-01-19 02:28:48 +0100

    backend: initail config

--- config.ts
+++ config.ts
@@ -1,12 +1,10 @@
-declare module "config" {
-    export default = {
-        // path of sqlite3 database file relaive to the project root
-        "db" : 'lauefer.db',
+export default {
+    // path of sqlite3 database file relaive to the project root
+    "db" : 'lauefer.db',
 
-        // path of the lof folder relative to the project root
-        "log_folder" : "log",
+    // path of the lof folder relative to the project root
+    "log_folder" : "log",
 
-        // is the appliaction in debbuging mode (true/false)
-        "debug" : true
-    };
+    // is the appliaction in debbuging mode (true/false)
+    "debug" : true
 };
