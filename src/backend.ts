dcfc1bd39a8f75d4ac84b1e7151f074384d79823
semklauke <privat@semklauke.de>
2020-01-19 01:48:26 +0100

    backend: avoid aduit message for vournabilitys. They all come from core-js or npx. we can check for audits on deployment

--- src/backend.ts
+++ src/backend.ts
@@ -1,1 +1,8 @@
+import express from 'express';
+import sqlite from 'sqlite';
+import winston from 'winston';
 
+import config from 'config';
+import { debugLogger, productionLogger, dbLogger } from './includes/logger';
+
+const logger: winston.Logger = config.debug ? debugLogger : productionLogger;
