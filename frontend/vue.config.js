bae4ed419065bfcc029eb40e5873d07891fa0629
semklauke <privat@semklauke.de>
2020-01-27 03:07:59 +0100

    frontend: added page login to the vue build

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,22 @@
 // vue.config.js
 module.exports = {
-  outputDir: '../dist',
-  assetsDir: 'static',
-  indexPath: 'index.html',
-  filenameHashing: true
+    pages: {
+        index: {
+            entry: 'src/main.ts',
+            template: 'public/index.html',
+            filename: 'index.html',
+            title: 'WaS 2020'
+        },
+        login: {
+            entry: 'src/login/main.ts',
+            template: 'public/login.html',
+            filename: 'login.html',
+            title: 'WaS 2020 Login'
+        }
+    },
+    
+    outputDir: '../dist',
+    assetsDir: 'static',
+    indexPath: 'index.html',
+    filenameHashing: true
 }
