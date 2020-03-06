e5c2e1e514a44061c8fb29f15c1f707a81580445
semklauke <privat@semklauke.de>
2020-03-06 11:49:32 +0100

    frontend: baseline app with router routes enabled and imported

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,96 @@
 <template>
-  <div id="app">
-  </div>
+<div id="app">
+    <b-navbar variant="light" toggleable="md" type="light" sticky>
+        <b-navbar-brand href="/">WaS 2020</b-navbar-brand>
+        <b-collapse is-nav id="WAS_mainNaBarCollapseWrapper">
+            <b-navbar-nav >
+                <b-nav-item :to="{ name: 'edit' }">Edit</b-nav-item>
+                <b-nav-item :to="{ name: 'overview' }">Overview</b-nav-item>
+                <b-nav-item :to="{ name: 'walker' }">Walker</b-nav-item>
+                <b-nav-item :to="{ name: 'admin' }">Admin</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+
+        <b-navbar-toggle target="WAS_mainNaBarCollapseWrapper"></b-navbar-toggle>
+    </b-navbar>
+    <transition
+        enter-active-class="animated fadeIn faster"
+        leave-active-class="animated fadeOut faster"
+    >
+        <router-view></router-view>
+    </transition>
+</div>
 </template>
 
+
 <style>
+
+@import url("../node_modules/animate.css/animate.min.css");
+
+.form-control:focus {
+    box-shadow: none !important;
+}
+
+html, body {
+    overflow: hidden;
+}
+#app {
+    overflow: hidden;
+}
+body {
+    background-color: white;
+    /*background-color: #E7E7E7;*/
+}
+.navbar {
+    box-shadow: 0px 3px 3px 0px rgba(50, 50, 50, 0.20) !important;
+}
+
+nav {
+
+    padding-top: 0px !important;
+    padding-bottom: 0px !important;
+    height: 50px !important;
+    max-height: 50px !important;
+
+}
+.navbar-nav, .nav-item, .nav-item a{
+    height: 50px !important;
+    max-height: 50px !important;
+}
+.nav-item a {
+    text-align: center !important;
+    line-height: 2.1rem !important;
+    font-size: 1.2rem !important;
+}
+
+.nav-item a {
+    transition: background-position .2s, box-shadow .4s, color 0.2s ease-out;    
+    background: var(--light);
+    background-size: 100% 200%;
+    background-image: linear-gradient(var(--light) 50%, var(--primary) 50%);
+
+}
+.nav-item a:not(.router-link-active):hover {
+    background-position: 0 100%;
+    color: white !important;
+}
+
+.router-link-active, .nav-item a:active, .nav-item a:focus {
+    background-position: 0 100% !important;
+    box-shadow: inset 1px 1px 8px 1px rgba(0,65,134,0.50);
+    color: white !important;
+}
+
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance:textfield;
+}
 
 </style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,15 +1,35 @@
+/// <reference types="vue" />
+/// <reference types="vue-router" />
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-//import Home from '../views/Home.vue';
+import { RouteConfig } from 'vue-router';
+
+import Edit from '@/views/Edit.vue';
+import EditWalker from '@/views/EditWalker.vue';
+const Overview = { template: '<div>Overview</div>' }
+const Walker = { template: '<div>Walker</div>' }
+const Admin = { template: '<div>Admin</div>' }
 
 Vue.use(VueRouter);
 
-const routes = [
-  
+const routes: RouteConfig[] = [
+    { 
+        path: '/edit', 
+        component: Edit, 
+        name: 'edit',
+        children: [
+            { path: ':id', component: EditWalker, name: 'edit-walker'}
+        ]
+    },
+    { path: '/overview', component: Overview, name: 'overview' },
+    { path: '/walker', component: Walker, name: 'walker' },
+    { path: '/admin', component: Admin, name: 'admin' }
 ];
 
 const router = new VueRouter({
   routes,
 });
+
+router.replace({ name: 'edit' });
 
 export default router;
