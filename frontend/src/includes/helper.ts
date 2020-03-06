648e1a4b8df75c6f68d9a44ed5b2b63440dab427
semklauke <privat@semklauke.de>
2020-03-06 11:54:12 +0100

    frontend: helper file with functions multiple files need. filterWalker: filters array of walker objects based on search string animatecss: helper function for animate.css

---
+++ frontend/src/includes/helper.ts
@@ -0,0 +1,68 @@
+import { Walker } from './../../../src/types';
+export default {
+    filterWalker: function(walker: Walker[], search: string) : Walker[] {
+        if (search == '' || search === '' || !search || search == null)
+            return walker;
+        let _walker: Walker[];
+        let _search: string = search.trim().toLowerCase();
+        let _search_split: string[] = _search.split(":");
+        if (_search_split.length === 1) {
+            _walker = walker.filter(function(w){
+                let name = (w.firstname+" "+w.lastname).toLowerCase();
+                if (name.indexOf(_search) !== -1)
+                    return w;
+            });
+        } else {
+            _search = _search_split[1];
+            let _class = _search_split[0];
+            _walker = walker.filter(function(w) {
+                if (w.class.toLowerCase().indexOf(_class) !== -1) {
+                    let name = (w.firstname+" "+w.lastname).toLowerCase();
+                    if (name.indexOf(_search) !== -1)
+                        return w;
+                } 
+            });
+        }
+        return _walker;
+    },
+    animateCSS: function(element: string, animationName: string, callback: () => void) {
+        const node: Element | null = document.querySelector(element);
+        if (node !== null) {
+            node.classList.add('animated', animationName);
+            function handleAnimationEnd(): void {
+                node.classList.remove('animated', animationName);
+                node.removeEventListener('animationend', handleAnimationEnd);
+
+                if (typeof callback === 'function') callback()
+            }
+            node.addEventListener('animationend', handleAnimationEnd);
+        }
+    }
+}
+// export default {
+//         filterWalker: function(walker, search) {
+//         if (search == '' || search === '' || !search || search == null)
+//             return walker;
+//         let _walker;
+//         let _search = search.trim().toLowerCase();
+//         let _search_split = _search.split(":");
+//         if (_search_split.length === 1) {
+//             _walker = walker.filter(function(w){
+//                 let name = (w.firstname+" "+w.lastname).toLowerCase();
+//                 if (name.indexOf(_search) !== -1)
+//                     return w;
+//             });
+//         } else {
+//             _search = _search_split[1];
+//             let _class = _search_split[0];
+//             _walker = walker.filter(function(w) {
+//                 if (w.class.toLowerCase().indexOf(_class) !== -1) {
+//                     let name = (w.firstname+" "+w.lastname).toLowerCase();
+//                     if (name.indexOf(_search) !== -1)
+//                         return w;
+//                 } 
+//             });
+//         }
+//         return _walker;
+//     }
+// }
