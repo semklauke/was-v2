3560f0a7b497f6a7015a53b4a98b7417e5b3ec1d
semklauke <privat@semklauke.de>
2020-03-06 11:52:05 +0100

    frontend: added axios interceptor to send login token for api requests

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,31 @@
 import Vue from 'vue';
+import './plugins/bootstrap-vue'
+import './plugins/vue-axios';
 import App from './App.vue';
 import router from './router';
+import cookie from 'cookie';
 
 Vue.config.productionTip = false;
 
-new Vue({
+let vm = new Vue({
   router,
   render: h => h(App),
 }).$mount('#app');
+
+//@ts-ignore
+vm.axios.interceptors.request.use(function (config: any) {
+    config.timeout = 1000;
+    config.validateStatus = function (status: number) {
+        return true
+    };
+    let sid: { [key: string] : string } = cookie.parse(document.cookie);
+    if (sid['connect.id']) {
+        config.headers.cookie += cookie.serialize('connect.id', sid['connect.id'], {
+            secure: true,
+        }) + ";";
+    }
+    return config;
+}, function (error: any) {
+    
+    return Promise.reject(error);
+});
